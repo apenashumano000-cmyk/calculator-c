43f340f3fb4d85bfc18c0088876216a85dbcf176
duzztheromantic <apenashumano000@gmail.com>
2026-03-24 19:52:29 -0300

    idk

--- main.c
+++ main.c
@@ -12,11 +12,6 @@ void clear_screen() {
 }
 
 int main() {
-    char ad = '+';
-    char sub = '-';
-    char mult = '*';
-    char div = '/';
-
     while (1) {
         printf("===============================\n");
         printf("     Calculadora Simples\n");
@@ -31,18 +26,18 @@ int main() {
         int request;
         char input_buffer[100];
         if (fgets(input_buffer, sizeof(input_buffer), stdin) == NULL) {
-            printf("Digite uma opção valida.\n");
+            printf("Digite uma opcao valida.\n");
             clear_screen();
             continue;
         }
         if (sscanf(input_buffer, "%d", &request) != 1) {
-            printf("Digite uma opção valida.\n");
+            printf("Digite uma opcao valida.\n");
             clear_screen();
             continue;
         }
 
         if (request < 1 || request > 5) {
-            printf("Digite uma opção valida.\n");
+            printf("Digite uma opcao valida.\n");
             clear_screen();
             continue;
         }
@@ -53,7 +48,7 @@ int main() {
         }
 
         double num1, num2;
-        printf("Digite o primeiro namero: ");
+        printf("Digite o primeiro numero: ");
         if (fgets(input_buffer, sizeof(input_buffer), stdin) == NULL || sscanf(input_buffer, "%lf", &num1) != 1) {
             printf("Entrada invalida para numero.\n");
             clear_screen();
@@ -70,22 +65,22 @@ int main() {
         switch (request) {
             case 1:
                 res = num1 + num2;
-                printf("Resultado: %.6g %c %.6g = %.6g\n", num1, ad, num2, res);
+                printf("Resultado: %.6g + %.6g = %.6g\n", num1, num2, res);
                 break;
             case 2:
                 res = num1 - num2;
-                printf("Resultado: %.6g %c %.6g = %.6g\n", num1, sub, num2, res);
+                printf("Resultado: %.6g - %.6g = %.6g\n", num1, num2, res);
                 break;
             case 3:
                 res = num1 * num2;
-                printf("Resultado: %.6g %c %.6g = %.6g\n", num1, mult, num2, res);
+                printf("Resultado: %.6g * %.6g = %.6g\n", num1, num2, res);
                 break;
             case 4:
                 if (num2 == 0) {
                     printf("Erro: Divisao por zero nao eh permitida.\n");
                 } else {
                     res = num1 / num2;
-                    printf("Resultado: %.6g %c %.6g = %.6g\n", num1, div, num2, res);
+                    printf("Resultado: %.6g / %.6g = %.6g\n", num1, num2, res);
                 }
                 break;
         }
@@ -95,14 +90,12 @@ int main() {
             printf("Erro: Resposta invalida.\n");
             break;
         }
-        // Remove trailing newline if present
         size_t len = strlen(input_buffer);
         if (len > 0 && input_buffer[len - 1] == '\n') {
             input_buffer[len - 1] = '\0';
         }
-        // Convert to lowercase
-        char replay = tolower(input_buffer[0]);
 
+        char replay = tolower(input_buffer[0]);
         if (replay != 's' && replay != 'n') {
             printf("Erro: Resposta invalida.\n");
             break;
@@ -111,6 +104,7 @@ int main() {
             printf("Obrigado por usar a calculadora! Ate a proxima.\n");
             break;
         }
+
         clear_screen();
     }
 
